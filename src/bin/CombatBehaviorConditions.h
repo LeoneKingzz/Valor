2d74c7a5aa24c5af95040529bdc21b57b2553863
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-05-16 04:48:55 +0100

    Update CombatBehaviorConditions.h

--- src/bin/CombatBehaviorConditions.h
+++ src/bin/CombatBehaviorConditions.h
@@ -2,21 +2,23 @@
 
 namespace Movement
 {
-	namespace Fallback
+	float get_FallbackDistance(RE::Character* me);
+	
+	namespace Dodging
 	{
-		float get_FallbackDistance(RE::Character* me);
-		float get_FallbackSpeed(RE::Character*);
 		uint32_t should(RE::Character*);
-		float get_FallbackWaitTime(RE::Character*);
-	}
-
-	namespace Circle
-	{
-		float get_CircleAngle(RE::Character* me);
 		uint32_t should_danger(RE::Character*);
+		uint32_t is_indanger(RE::Character*);
 	}
 
+
+
+
+
+
+	
+	float get_FallbackWaitTime(RE::Character*);
+	float get_CircleAngle(RE::Character* me);
 	uint32_t low_stamina(RE::Character*);
-	uint32_t is_indanger(RE::Character*);
 	bool is_outofrange(void*);
 }
