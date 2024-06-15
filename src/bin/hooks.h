c27a830ad1ea028153444bfffefb503498be2d11
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-06-15 05:33:40 +0100

    lool

--- src/bin/hooks.h
+++ src/bin/hooks.h
@@ -14,67 +14,24 @@ namespace hooks
 	class on_animation_event
 	{
 	public:
-		// static void install()
-		// {
-		// 	REL::Relocation<uintptr_t> AnimEventVtbl_NPC{ RE::VTABLE_Character[2] };
-		// 	REL::Relocation<uintptr_t> AnimEventVtbl_PC{ RE::VTABLE_PlayerCharacter[2] };
-
-		// 	_ProcessEvent_NPC = AnimEventVtbl_NPC.write_vfunc(0x1, ProcessEvent_NPC);
-		// 	_ProcessEvent_PC = AnimEventVtbl_PC.write_vfunc(0x1, ProcessEvent_PC);
-		// 	logger::info("hook:on_animation_event");
-		// }
 		static void install()
 		{
-			REL::Relocation<uintptr_t> pcPtr{ RE::VTABLE_PlayerCharacter[2] };
-			HookSink(pcPtr.address());
-			REL::Relocation<uintptr_t> npcPtr{ RE::VTABLE_Character[2] };
-			HookSink(npcPtr.address());
+			REL::Relocation<uintptr_t> AnimEventVtbl_NPC{ RE::VTABLE_Character[2] };
+			REL::Relocation<uintptr_t> AnimEventVtbl_PC{ RE::VTABLE_PlayerCharacter[2] };
 
-			// REL::Relocation<uintptr_t> AnimEventVtbl_NPC{ RE::VTABLE_Character[2] };
-			// REL::Relocation<uintptr_t> AnimEventVtbl_PC{ RE::VTABLE_PlayerCharacter[2] };
-
-			// _ProcessEvent_NPC = AnimEventVtbl_NPC.write_vfunc(0x1, ProcessEvent_NPC);
-			// _ProcessEvent_PC = AnimEventVtbl_PC.write_vfunc(0x1, ProcessEvent_PC);
+			_ProcessEvent_NPC = AnimEventVtbl_NPC.write_vfunc(0x1, ProcessEvent_NPC);
+			_ProcessEvent_PC = AnimEventVtbl_PC.write_vfunc(0x1, ProcessEvent_PC);
 			logger::info("hook:on_animation_event");
 		}
 
 	private:
-		template <class Ty>
-		static Ty SafeWrite64Function(uintptr_t addr, Ty data)
-		{
-			DWORD oldProtect;
-			void* _d[2];
-			memcpy(_d, &data, sizeof(data));
-			size_t len = sizeof(_d[0]);
+		static inline void ProcessEvent(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
 
-			VirtualProtect((void*)addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
-			Ty olddata;
-			memset(&olddata, 0, sizeof(Ty));
-			memcpy(&olddata, (void*)addr, len);
-			memcpy((void*)addr, &_d[0], len);
-			VirtualProtect((void*)addr, len, oldProtect, &oldProtect);
-			return olddata;
-		}
+		static EventResult ProcessEvent_NPC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
+		static EventResult ProcessEvent_PC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
 
-		typedef RE::BSEventNotifyControl (on_animation_event::*FnProcessEvent)(RE::BSAnimationGraphEvent& a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* dispatcher);
-
-		RE::BSEventNotifyControl HookedProcessEvent(RE::BSAnimationGraphEvent& a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* src);
-
-		static void HookSink(uintptr_t ptr)
-		{
-			FnProcessEvent fn = SafeWrite64Function(ptr + 0x8, &on_animation_event::HookedProcessEvent);
-			fnHash.insert(std::pair<uint64_t, FnProcessEvent>(ptr, fn));
-		}
-		// static inline void ProcessEvent(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
-
-		// static EventResult ProcessEvent_NPC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
-		// static EventResult ProcessEvent_PC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource);
-
-		// static inline REL::Relocation<decltype(ProcessEvent_NPC)> _ProcessEvent_NPC;
-		// static inline REL::Relocation<decltype(ProcessEvent_NPC)> _ProcessEvent_PC;
-
-	protected:
-		static std::unordered_map<uint64_t, FnProcessEvent> fnHash;
+		static inline REL::Relocation<decltype(ProcessEvent_NPC)> _ProcessEvent_NPC;
+		static inline REL::Relocation<decltype(ProcessEvent_NPC)> _ProcessEvent_PC;
 	};
 	
 	typedef uintptr_t ptr_CombatPath;
